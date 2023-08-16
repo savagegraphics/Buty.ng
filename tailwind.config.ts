3f0fffaf07aa4cedcf3b88adf116136e10ef8d58
Elijah Ohiwerei <elijahohimairei@gmail.com>
2023-08-16 21:29:18 +0100

    add necessary fonts

---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        aeonik: ["var(--font-aeonik)"],
+        madeMellow: ["var(--font-madeMellow)"],
+      },
+      colors: {
+        eaziDark: "#515251",
+        eaziGreen: "#11453B",
+      },
+      boxShadow: {
+        button: "0px 8px 16px 0px rgba(17, 69, 59, 0.20)",
+        container: "0px 20px 48px 0px rgba(170, 170, 170, 0.29)",
+      },
+      screens: {
+        "3xl": "1840px",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
